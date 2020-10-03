489f37207a7f53fb4c301aa8a12bc84f60fab1ad
Lauren <68979433+renpapers@users.noreply.github.com>
2020-10-03 16:28:53 +1000

    Add files via upload

---
+++ Processing Sketches/AT2_trial4/p5js-temp-AT2_trial42921551799679930974.js
@@ -0,0 +1,59 @@
+var xPosition, yPosition, size, rand; 
+var wigglyStars = []; //group/bag of stars, what stars we have, where they are on the screen and what size they are 
+var fade; 
+var fadeAmount = 1
+
+function setup() {
+createCanvas(windowWidth, windowHeight);
+background (0);
+textSize(30);
+textFont("Times"); //serif font
+fade = 0
+}
+
+
+function draw() {
+  background(0); //this would redraw the background every loop 
+  fill (255, 253, 234);
+text ('When I write Im kind of like a god, creating everything. I create what looks real to me.', 400, 600); 
+text ('- HM', 410, 800); 
+if (fade<0) fadeAmount=1;
+if (fade>255) fadeAmount=-10; 
+fade+= fadeAmount;
+print(fade);
+fill (255, 253, 234,);
+for (var i=0; i<wigglyStars.length; i++){
+  wigglyStars[i].display(); //this displays the stars/letters. for each object in wigglystars, wiggleletters 'i display'.
+}
+
+var sparkle = {
+  locationX: random(windowWidth),
+  locationY: random (windowHeight),
+  size: random(1,8)
+}
+fill (255, 253, 234,);
+noStroke();
+ellipse (sparkle.locationX, sparkle.locationY, sparkle.size, sparkle.size);
+
+}
+
+
+function mousePressed(){
+  size = int(random(6,100)); //int = integer. Randomising size from 6-100. 
+  wigglyStars.push(new Wiggle (mouseX, mouseY, size)); //new wiggle = new character
+}
+class Wiggle {  //note: classes generally start with a capital letter. Class instantiates and move/wiggle the stars
+constructor (x, y, size) { //wishes wiggle into existence. 'x, y and size' are var's you created
+  this.x = x;
+  this.y = y;
+  this.textSize = size; //all of this sets up the new object (aka star) with a new x, y and size value. 
+}
+display() {
+  ellipse (this.x, this.y, this.textSize, this.textSize); 
+  textSize(this.textSize);
+  text('HARUKI MURAKAMI', this.x, this.y); //an action we add to the 'class'. Will write the stars to the screen. Takes info abt the size, applies it to the font and writes it to the screen. 
+
+}
+
+
+}
